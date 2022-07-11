776521a57c10f8b48243db2f0e331724d20e353f
dadius79 <david.sule@outlook.com>
2022-07-11 23:07:39 +0300

    Added pop method

--- linkedLists.js
+++ linkedLists.js
@@ -28,6 +28,21 @@ class LinkedList{
 
     pop(){
         if(!this.head) return undefined;
+        let temp = this.head;
+        let pre = this.head;
+
+        while(temp.next){
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
     }
 }
 
